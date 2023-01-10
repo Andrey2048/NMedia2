082bcc20c3dceb418152c9b8fbf123aad61fefda
Andrey Lobanov <andrey922gmail.com>
2023-01-11 00:08:48 +0300

    Homework 2-1 (Like, Share) done

---
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -0,0 +1,35 @@
+package ru.netology.nmedia.dto
+
+import java.math.RoundingMode
+import java.text.DecimalFormat
+
+data class Post(
+    val id: Int,
+    val author: String,
+    val published: String,
+    val content: String,
+    var likedByMe: Boolean,
+    var likes: Int,
+    var shares: Int,
+    var views: Int
+)
+
+fun countView(n: Int): String {
+    var df = DecimalFormat("#.#K")
+    df.roundingMode = RoundingMode.DOWN
+    val out = when (n) {
+        in 0..999 -> n.toString()
+        in 1_000..9_999 -> df.format(n / 1_000.0)
+        in 10_000..999_999 -> {
+            df = DecimalFormat("#K")
+            df.roundingMode = RoundingMode.DOWN
+            df.format(n / 1_000.0)
+        }
+        else -> {
+            df = DecimalFormat("#.#M")
+            df.roundingMode = RoundingMode.DOWN
+            df.format(n / 1_000_000.0)
+        }
+    }
+    return out
+}
