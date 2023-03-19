497f07c44bc0bb0d7a9e45c9df1e16b591bcd854
Andrey Lobanov <andrey922gmail.com>
2023-03-19 23:37:54 +0300

    Homework "Network requests" - 2

--- app/src/main/java/ru/netology/nmedia/entity/PostEntity.kt
+++ app/src/main/java/ru/netology/nmedia/entity/PostEntity.kt
@@ -9,19 +9,17 @@ data class PostEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Long,
     val author: String,
-    val published: String,
     val content: String,
-    var video: String? = null,
-    var likedByMe: Boolean,
-    var likes: Int = 0,
-    var shares: Int = 0,
-    var views: Int = 0
+    val published: String,
+    val likedByMe: Boolean,
+    val likes: Int = 0,
 ) {
-    fun toDto() = Post(id, author, published, content, video, likedByMe, likes, shares, views)
+    fun toDto() = Post(id, author, content, published, likedByMe, likes)
 
     companion object {
         fun fromDto(dto: Post) =
-            PostEntity(dto.id, dto.author, dto.published, dto.content, dto.video, dto.likedByMe, dto.likes, dto.shares, dto.views)
+            PostEntity(dto.id, dto.author, dto.content, dto.published, dto.likedByMe, dto.likes)
 
     }
 }
+
